673da85f22b56fdb873f9f4b693ed7cc09182184
khoinguyen <36569441+chimnonvuive@users.noreply.github.com>
2022-01-11 10:11:08 +0700

    update 11/1

---
+++ EPOS4/Epos4Windows/motorInfoStruct.h
@@ -0,0 +1,11 @@
+/* Model Code Variants */
+#ifndef DEFINED_TYPEDEF_FOR_motorInfo_
+#define DEFINED_TYPEDEF_FOR_motorInfo_
+
+typedef struct {
+  double Angle;
+  double Speed;
+  double Current;
+} motorInfo;
+
+#endif
