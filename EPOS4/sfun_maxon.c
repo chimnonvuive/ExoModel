2bcac5f78739c138e46277174da2b7f230efbab8
khoinguyen <36569441+chimnonvuive@users.noreply.github.com>
2022-01-20 21:37:50 +0700

    update 20/1

--- EPOS4/sfun_maxon.c
+++ EPOS4/sfun_maxon.c
@@ -129,8 +129,8 @@ static void mdlInitializeSizes(SimStruct *S) {
     /* options */
     ssSetOptions(S,
         SS_OPTION_WORKS_WITH_CODE_REUSE |
-        SS_OPTION_EXCEPTION_FREE_CODE |
-        SS_OPTION_USE_TLC_WITH_ACCELERATOR
+        SS_OPTION_EXCEPTION_FREE_CODE
+//         SS_OPTION_USE_TLC_WITH_ACCELERATOR
         );
 }
 
