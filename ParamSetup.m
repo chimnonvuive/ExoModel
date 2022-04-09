e54e410961252921c8c4a0bc52b02899a762c320
khoinguyen <36569441+chimnonvuive@users.noreply.github.com>
2022-04-09 19:16:06 +0700

    update

---
+++ ParamSetup.m
@@ -0,0 +1,28 @@
+%% Testing
+% Model DSMC_params was manually simulated and analyzed. The real motor was
+% used for validation of control algorithm. The errors stored in variable
+% xs were also recorded for analysis purposes. The parameters were
+% commented because this phase was finished 
+load DSMC_params
+load motorModel
+
+Ts = 0.015;
+nodeID = 4;
+N = uint8(10);
+%%
+Phi = single(mModel{nodeID,3}.A);
+Gam = single(mModel{nodeID,3}.B);
+C   = single(Cs(nodeID,:));
+del = single(dels(nodeID));
+eta = single(etas(nodeID));
+gamm= single(gamms(nodeID));
+qT  = single(qTs(nodeID));
+vphi= single(vphis(nodeID));
+xiT = single(xiTs(nodeID));
+%%
+i1 = timeseries(single([sm_posn.data(:,nodeID)*100, sm_veln.data(:,nodeID)*100]),sm_posn.time);
+i2 = timeseries(single(zeros(length(sm_posn.time),1)),sm_posn.time);
+i4 = timeseries(single([sm_posn.data*100, sm_veln.data*100]),sm_posn.time);
+% title(['Motor ',num2str(i)])
+%     xlabel('e_1 (deg)'), ylabel('e_2 (rpm)')
+    
